2f0ba19efac24add0da1d5e1d476fc82505f0eff
Henry Gu <70784275+henryhankai@users.noreply.github.com>
2021-11-14 13:32:47 -0800

    Add files via upload

---
+++ MakeEssence.py
@@ -0,0 +1,48 @@
+import pyautogui
+import psutil
+from time import sleep
+import sys
+
+max_iter = int(sys.argv[1])
+
+for i in range(max_iter):
+    try:
+        pyautogui.locateOnScreen('assets/shop.png', confidence=0.8)
+    except:
+        print("Shop not found, please open the token shop first")
+        break
+
+    flag = None
+    for i in range(3):
+        try:
+            x, y = pyautogui.locateCenterOnScreen('assets/essence.png', confidence=0.8)
+            pyautogui.click(x, y)
+            flag = True
+            break
+        except:
+            try:
+                x, y = pyautogui.locateCenterOnScreen('assets/shop.png', confidence=0.8)
+                pyautogui.click(x, y)
+                pyautogui.moveTo(x, y + 100)
+                pyautogui.scroll(-5000)
+                flag = False
+            except:
+                flag = False
+                break
+
+    if flag:
+        sleep(3)
+    else:
+        print("Something went wrong with finding the essence icon, terminating")
+        break
+
+    try:
+        x, y = pyautogui.locateCenterOnScreen('assets/forge.png', confidence=0.8)
+        pyautogui.click(x, y)
+        sleep(3)
+        x, y = pyautogui.locateCenterOnScreen('assets/add.png', confidence=0.8)
+        pyautogui.click(x, y)
+        sleep(3)
+    except:
+        print("Something went wrong with forging, terminating")
+        break
