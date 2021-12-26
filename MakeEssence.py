dcb0bd22f47beecf5bf1318c6c7371cf2f750ee6
Henry Gu <70784275+henryhankai@users.noreply.github.com>
2021-12-26 14:30:06 -0800

    Add files via upload

--- MakeEssence.py
+++ MakeEssence.py
@@ -3,6 +3,11 @@ import psutil
 from time import sleep
 import sys
 
+if sys.argv[2] == 'essence':
+    type = 'assets/essence.png'
+elif sys.argv[2] == 'key':
+    type = 'assets/key.png'
+
 max_iter = int(sys.argv[1])
 
 for i in range(max_iter):
@@ -15,24 +20,25 @@ for i in range(max_iter):
     flag = None
     for i in range(3):
         try:
-            x, y = pyautogui.locateCenterOnScreen('assets/essence.png', confidence=0.8)
+            x, y = pyautogui.locateCenterOnScreen(type, confidence=0.8)
             pyautogui.click(x, y)
             flag = True
             break
         except:
-            flag = False
             try:
                 x, y = pyautogui.locateCenterOnScreen('assets/shop.png', confidence=0.8)
                 pyautogui.click(x, y)
                 pyautogui.moveTo(x, y + 100)
                 pyautogui.scroll(-5000)
+                flag = False
             except:
+                flag = False
                 break
 
     if flag:
         sleep(3)
     else:
-        print("Something went wrong with finding the essence icon, terminating")
+        print("Something went wrong with finding the icon, terminating")
         break
 
     try:
